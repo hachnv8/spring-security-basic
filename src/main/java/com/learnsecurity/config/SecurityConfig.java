2f2255a77766327d1b9328c7dd5e22f2f025119c
hachnv8 <hachnv8@gmail.com>
2024-07-11 16:51:02 +0700

    add preauthorize

--- src/main/java/com/learnsecurity/config/SecurityConfig.java
+++ src/main/java/com/learnsecurity/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.learnsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
